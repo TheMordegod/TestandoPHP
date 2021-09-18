baf3e2d88695b1c6b19ab92ed720dd5db70a9b30
Levy <levysilva72@gmail.com>
2021-09-18 17:20:17 -0300

    Adicionado restrições de input

--- script.php
+++ script.php
@@ -2,11 +2,56 @@
     $nome = $_POST['nome'];
     $idade = $_POST['idade'];
 
-    if($idade < 18)
+    $erro = false;
+
+    if(checarNome($nome) || checarIdade($idade))
     {
-        print("O nadador $nome não poderá competir pois possui apenas $idade anos");
+        return;
     }
-    else{
-        print("<h1>Bem vindo</h1><h2>$nome!</h2>");
+
+    print("<h1>Bem vindo $nome!</h1>");
+
+
+    function checarNome($nome)
+    {
+        $erro = false;
+
+        if(empty($nome))
+        {
+            print("O Nome não pode ser vazio! ");
+            return $erro = true;
+        }
+
+        if(strlen($nome) < 3 || strlen($nome) > 40 && $nome != null)
+        {
+            echo 'O nome deve conter no maximo 40 caracteres e no minimo 3';
+            return $erro = true;
+        }   
+
+        return false;    
+    }
+
+    function checarIdade(string $idade)
+    {
+        $erro = false;
+
+        if(empty($idade))
+        {
+            print("A idade deve conter um valor!");
+            return $erro = true;
+        }
+
+        if(!is_numeric($idade))
+        {
+            echo 'A idade não deve conter letras';
+            return $erro = true;
+        }
+
+        if($idade < 18)
+        {
+            print("você não poderá competir pois possui apenas $idade anos");
+            return $erro = true;
+        }
+        return false;
     }
 ?>
